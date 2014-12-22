3e3c6c2754e743cec46b89084411d0161bff191a
IreneZaman <IreneZ473639@gmail.com>
2014-12-21 19:02:14 -0500

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The purpose of the following functions are to cache 
+# the inverse of a matrix, rather than having to compute 
+# it repeatedly which is not as efficient. 
 
-## Write a short comment describing this function
+# The makeCacheMatrix function, I hope, will be able to set and 
+# also get the value of a matrix and then set and get the value 
+# of the inverse of said matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <-function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function should return the inverse 
+## of the matrix. The function will assume that the matrix is 
+## always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  
+  if(!is.null(m)) {
+    message("retrieving cache")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  
+  x$setinverse(m)
+  m
 }
